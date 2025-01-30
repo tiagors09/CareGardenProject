7fb1057a3fa313ef5325b603efc467e469ced55f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-01-29 21:04:48 -0300

    chore: Adicionar projeto backend em Java + Spring

---
+++ backend/caregarden/src/main/java/br/com/tiagors09/caregarden/CaregardenApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagors09.caregarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaregardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaregardenApplication.class, args);
+	}
+
+}
---
+++ backend/caregarden/src/test/java/br/com/tiagors09/caregarden/CaregardenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagors09.caregarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaregardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
