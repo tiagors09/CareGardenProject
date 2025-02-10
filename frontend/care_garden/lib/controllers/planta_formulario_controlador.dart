b7ba5ff3673f116964c8afd318e4e9447f37e36a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-02-10 20:12:21 -0300

    atualização projeto

--- frontend/care_garden/lib/controllers/planta_formulario_controlador.dart
+++ frontend/care_garden/lib/controllers/planta_formulario_controlador.dart
@@ -1,1 +1,13 @@
-class PlantaFormularioControlador {}
+import 'package:flutter/material.dart';
+
+class PlantaFormularioControlador {
+  Future<DateTime?> abrirModalSelecaoData(BuildContext ctx) {
+    return showDatePicker(
+      context: ctx,
+      firstDate: DateTime.utc(DateTime.now().year, 01, 01),
+      lastDate: DateTime.utc(DateTime.now().year, 12, 31),
+    );
+  }
+
+  void selecionarImage() {}
+}
--- frontend/care_garden/lib/views/screens/planta_formulario.dart
+++ frontend/care_garden/lib/views/screens/planta_formulario.dart
@@ -1,5 +1,7 @@
 import 'package:care_garden/controllers/planta_formulario_controlador.dart';
+import 'package:care_garden/models/planta.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class PlantaFormulario extends StatefulWidget {
   final PlantaFormularioControlador controlador;
@@ -14,15 +16,76 @@ class PlantaFormulario extends StatefulWidget {
 }
 
 class _PlantaFormularioState extends State<PlantaFormulario> {
+  DateTime? dataAquisicao;
+  final _formKey = GlobalKey<FormState>();
+  final _formData = <String, dynamic>{};
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final planta = ModalRoute.of(context)?.settings.arguments;
+
+    if (_formData.isEmpty && planta != null) {
+      planta as Planta;
+      _formData['id'] = planta.id;
+      _formData['caminhoFoto'] = planta.caminhoFoto;
+      _formData['nome'] = planta.nome;
+      _formData['dataDeAquisicao'] = planta.dataDeAquisicao;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Forumlário Planta'),
+        title: Text('Formulário Planta'),
       ),
-      body: Center(
-        child: Text(
-          'Formulário aqui',
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(24),
+          children: [
+            TextButton.icon(
+              icon: Icon(Icons.image),
+              onPressed: widget.controlador.selecionarImage,
+              label: Text('Escolher uma imagem'),
+            ),
+            TextFormField(
+              initialValue: _formData['nome'] ?? '',
+              decoration: InputDecoration(
+                label: Text('Nome da planta'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 8,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    dataAquisicao != null
+                        ? DateFormat('dd MMM yyyy').format(dataAquisicao!)
+                        : 'Sem data definida',
+                  ),
+                  TextButton.icon(
+                    icon: Icon(Icons.calendar_month),
+                    onPressed: () {
+                      widget.controlador.abrirModalSelecaoData(context).then(
+                            (novaData) => setState(
+                              () {
+                                dataAquisicao = novaData;
+                              },
+                            ),
+                          );
+                    },
+                    label: Text('Data de aquisição da planta'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
